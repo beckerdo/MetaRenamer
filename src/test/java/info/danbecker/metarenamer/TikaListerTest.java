0070971c5a63c9e859bf0c5450733f8ad7e549b6
unknown <dabecker@L-AUN-00559147.corp.ebay.com>
2014-08-01 16:03:11 -0500

    Move some demos to unit tests

---
+++ src/test/java/info/danbecker/metarenamer/TikaListerTest.java
@@ -0,0 +1,50 @@
+package info.danbecker.metarenamer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.tika.cli.TikaCLI;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TikaListerTest {
+	
+	@SuppressWarnings("unused")
+	private static Logger logger = LoggerFactory.getLogger(TikaListerTest.class);
+	
+	@Test
+    public void testVersion() throws Exception {
+		TikaCLI.main(new String[]{"--version"});
+	}
+
+	@Test
+    public void testHelp() throws Exception {
+		TikaCLI.main(new String[]{"--help"});
+	}
+
+	@Test
+    public void testParsers() throws Exception {
+		TikaCLI.main(new String[]{"--list-parsers"});
+	}
+
+	@Test
+    public void testParserDetails() throws Exception {
+		TikaCLI.main(new String[]{"--list-parser-details"});
+	}
+
+	@Test
+    public void testDetectors() throws Exception {
+		TikaCLI.main(new String[]{"--list-detectors"});
+	}
+
+	@Test
+    public void testMetadataModels() throws Exception {
+		TikaCLI.main(new String[]{"--list-met-models"});
+	}
+
+	@Test
+    public void testSupportedTypes() throws Exception {
+		TikaCLI.main(new String[]{"--list-supported-types"});
+	}
+
+}
